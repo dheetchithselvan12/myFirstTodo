f47f263be57acde12aefae57bd9e12c3cd269fef
dheetchithselvan12 <dheetchithselvan12@gmail.com>
2025-05-25 12:57:45 +0530

    Added Edit button to list

--- scripts/script.js
+++ scripts/script.js
@@ -9,16 +9,45 @@ addbtn.addEventListener('click', () =>{
 
     if(textTask !== ""){
         const li = document.createElement("li");
-        li.innerText = textTask;
+
+        const span = document.createElement("span");
+        span.textContent = textTask
 
         // Creat remove Button with list
        const removebtn = document.createElement("button");
-       removebtn.innerText = "-";
-       removebtn.classList.add("child-btn")
-
+       removebtn.textContent = "-";
+       removebtn.classList.add("child-btn");
        removebtn.addEventListener("click", () =>(li.remove()));                 //remove functionality
 
+    //    create Edit button with list
+    const editbtn = document.createElement("buttion");
+    editbtn.textContent = "Edit";
+    editbtn.classList.add("child-btn");
+    editbtn.addEventListener("click", () => {
+
+        if(editbtn.textContent === "Edit"){
+            const modifyText = document.createElement("input");
+            modifyText.value = span.textContent;
+            li.insertBefore(modifyText, span);
+            li.removeChild(span);
+            editbtn.textContent = "Save";
+
+        }
+        else {
+            const modifyText = li.querySelector("input");
+            const newspan = document.createElement("span");
+            newspan.textContent = modifyText.value;
+            li.insertBefore(newspan, modifyText);
+            li.removeChild(modifyText);
+            editbtn.textContent = "Edit";
+        }
+
+    });
+
+    // All child elements are added to list
+       li.appendChild(span)
        li.appendChild(removebtn);
+       li.appendChild(editbtn);
 
         document.getElementById("task-list").appendChild(li);
 
