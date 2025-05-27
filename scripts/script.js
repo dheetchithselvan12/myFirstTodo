0f2d9d1fb201d7e2e6dc08df4751f958b8114f36
dheetchithselvan12 <dheetchithselvan12@gmail.com>
2025-05-27 13:29:53 +0530

    Check box added: when the taks is clicked

--- scripts/script.js
+++ scripts/script.js
@@ -20,12 +20,27 @@ addbtn.addEventListener('click', () =>{
         span.textContent = textTask
 
         // Creat remove Button with list
+
        const removebtn = document.createElement("button");
        removebtn.textContent = "-";
        removebtn.classList.add("child-btn");
        removebtn.addEventListener("click", () =>(li.remove()));                 //remove functionality
 
+    //    check task done
+
+    let check = document.createElement("button");
+    check.innerHTML = "<i class='bx bx-radio-circle'></i> ";
+    check.classList.add("check-btn");
+    check.addEventListener("click", () => {
+        check.innerHTML = "<i class='bx bx-radio-circle-marked'></i>";
+        li.querySelector("span").style.textDecorationLine = "line-through",check.style.opacity = "0.4", span.style.opacity = "0.4";
+
+
+    });
+    
+
     //    create Edit button with list
+
     const editbtn = document.createElement("buttion");
     editbtn.textContent = "Edit";
     editbtn.classList.add("child-btn");
@@ -34,7 +49,6 @@ addbtn.addEventListener('click', () =>{
         const inputEdit = li.querySelector("input");
 
         if(!inputEdit){                                    // edit Button functionality
-
             const span = li.querySelector("span");
             const modifyText = document.createElement("input");
             modifyText.value = span.textContent;
@@ -44,17 +58,21 @@ addbtn.addEventListener('click', () =>{
 
         }
         else {
-
             const newspan = document.createElement("span");
             newspan.textContent = inputEdit.value;
             li.insertBefore(newspan, inputEdit);
             li.removeChild(inputEdit);
             editbtn.textContent = "Edit";
+
+            // edit after task done
+            li.querySelector("span").style.textDecorationLine = "line-through";
+            li.querySelector("span").style.opacity = "0.4"
         }
 
     });
 
     // All child elements are added to list
+       li.appendChild(check)
        li.appendChild(span)
        li.appendChild(removebtn);
        li.appendChild(editbtn);
